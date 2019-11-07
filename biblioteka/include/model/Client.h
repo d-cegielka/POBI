a9177203b81b69095ab4e4ad6ffed6ec36a4ab7c
Dominik Cegiełka <224478@edu.p.lodz.pl>
2019-11-07 17:35:45 +0100

    utworzenie klasy Client oraz dodanie prywatnych pól klasy

---
+++ biblioteka/include/model/Client.h
@@ -0,0 +1,23 @@
+//
+// Created by Dominik Cegiełka <224478@edu.p.lodz.pl> on 07.11.2019.
+//
+
+#ifndef POBIPROJECT_CLIENT_H
+#define POBIPROJECT_CLIENT_H
+
+#include <string>
+
+using namespace std;
+
+class Client {
+private:
+    string firstName;
+    string lastName;
+    string personalID;
+    string address;
+public:
+
+};
+
+
+#endif //POBIPROJECT_CLIENT_H
---
+++ biblioteka/src/model/Client.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Dominik Cegiełka <224478@edu.p.lodz.pl> on 07.11.2019.
+//
+
+#include "../../include/model/Client.h"
