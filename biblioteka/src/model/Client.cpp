e45f6a5705b2472db508ddb84324c917d21bde78
Kamil Zarych <224546@edu.p.lodz.pl>
2019-11-08 20:56:54 +0100

    modyfikacja konstruktora parametrowego, destruktora, metody clientInfo(), metod ustawiających wartości pól adresowych klasy Client

--- biblioteka/src/model/Client.cpp
+++ biblioteka/src/model/Client.cpp
@@ -7,37 +7,26 @@
 
 using namespace std;
 
-Client::Client(const string &firstName, const string &lastName, const string &personalId) : firstName(firstName),
-                                                                                            lastName(lastName),
-                                                                                            personalID(personalId) {
-    cout << "Konstruktor parametrowy Client zostal wywolany" << endl;
+Client::Client(const string &firstName, const string &lastName, const string &personalId,
+               const string &registredAddress_street,  const string &registredAddress_number,
+               const string &address_street, const string &address_number) :
+               firstName(firstName),
+               lastName(lastName),
+               personalID(personalId) {
+    registredAddress = new Address(registredAddress_street,registredAddress_number);
+    address = new Address(address_street,address_number);
 }
 
 Client::~Client() {
-    cout << "Destruktor Client zostal wywolany" << endl;
+    delete registredAddress;
+    delete address;
 }
 
 string Client::clientInfo() {
-    if((registredaddress!=NULL) && (address!=NULL)) {
         return "Imie i nazwisko: " + firstName + " " + lastName +
-               "\nPESEL: " + personalID +
-               "\nAdres zameldowania: " + registredaddress->getStreet() + " " + registredaddress->getNumber() +
-               "\nAdres zamieszkania: " + address->getStreet() + " " + address->getNumber();
-    }
-    else if(registredaddress!=NULL) {
-        return "Imie i nazwisko: " + firstName + " " + lastName +
-               "\nPESEL: " + personalID +
-               "\nAdres zameldowania: " + registredaddress->getStreet() + " " + registredaddress->getNumber();
-    }
-    else if(address!=NULL) {
-        return "Imie i nazwisko: " + firstName + " " + lastName +
-               "\nPESEL: " + personalID +
-               "\nAdres zamieszkania: " + address->getStreet() + " " + address->getNumber();
-    }
-    else {
-        return "Imie i nazwisko: " + firstName + " " + lastName +
-               "\nPESEL " + personalID;
-    }
+        "\nPESEL: " + personalID +
+        "\nAdres zameldowania: " + registredAddress->getStreet() + " " + registredAddress->getNumber() +
+        "\nAdres zamieszkania: " + address->getStreet() + " " + address->getNumber();
 }
 
 const string &Client::getFirstName() const {
@@ -52,10 +41,12 @@ const string &Client::getPersonalId() const {
     return personalID;
 }
 
-void Client::setAddress(Address *address) {
-    Client::address = address;
+void Client::setAddress(string street, string number) {
+    address->setStreet(street);
+    address->setNumber(number);
 }
 
-void Client::setRegistredaddress(Address *registredaddress) {
-    Client::registredaddress = registredaddress;
+void Client::setRegistredAddress(string street, string number) {
+    registredAddress->setStreet(street);
+    registredAddress->setNumber(number);
 }
--- program/src/main.cpp
+++ program/src/main.cpp
@@ -4,20 +4,13 @@
 using namespace std;
 
 int main() {
-    Client k1("Waldemar","Nowak","92875697851");
-    Client k2("Jan","Kowalski","87110701881");
-    cout<<k1.clientInfo()<<endl;
-    cout<<k2.clientInfo()<<endl;
-    Address adres("Warszawska","59A");
-
-    k1.setAddress(&adres);
-    k2.setAddress(&adres);
-
+    Client k1("Waldemar","Nowak","92875697851", "Warszawska", "22","Batorego","55G");
+    Client k2("Jan","Kowalski","87110701881","Spokojna","1","Głośna","12");
     cout<<k1.clientInfo()<<endl;
     cout<<k2.clientInfo()<<endl;
 
-    adres.setStreet("Poznańska");
-    adres.setNumber("66");
+    k1.setAddress("Gdańska","110");
+    k2.setAddress("Błotna","55");
 
     cout<<k1.clientInfo()<<endl;
     cout<<k2.clientInfo()<<endl;
