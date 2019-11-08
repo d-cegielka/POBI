0afb7f2c0324086d2b701cfa6d99c8800b8f014f
Kamil Zarych <224546@edu.p.lodz.pl>
2019-11-08 16:29:13 +0100

    utworzenie obiektu klasy Address oraz przypisanie adresu obu klientom

--- program/src/main.cpp
+++ program/src/main.cpp
@@ -8,7 +8,13 @@ int main() {
     Client k2("Jan","Kowalski","87110701881");
     cout<<k1.clientInfo()<<endl;
     cout<<k2.clientInfo()<<endl;
+    Address adres("Warszawska","59A");
 
+    k1.setAddress(&adres);
+    k2.setAddress(&adres);
+
+    cout<<k1.clientInfo()<<endl;
+    cout<<k2.clientInfo()<<endl;
 
     return 0;
 }
