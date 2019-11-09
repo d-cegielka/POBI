ef2eb14892f9f9ff4ff2fdc26f10e310b4f595aa
Dominik Cegiełka <224478@edu.p.lodz.pl>
2019-11-09 16:21:39 +0100

    utworzenie klasy Vehicle

---
+++ biblioteka/include/model/Vehicle.h
@@ -0,0 +1,24 @@
+//
+// Created by Dominik Cegiełka <224478@edu.p.lodz.pl> on 09.11.2019.
+//
+
+#ifndef POBIPROJECT_VEHICLE_H
+#define POBIPROJECT_VEHICLE_H
+
+#include <string>
+
+using namespace std;
+
+class Vehicle {
+private:
+    const string id;
+    const int baseRentPrice;
+public:
+    Vehicle(const string &id, const int baseRentPrice);
+    virtual ~Vehicle();
+
+
+};
+
+
+#endif //POBIPROJECT_VEHICLE_H
---
+++ biblioteka/src/model/Vehicle.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Dominik Cegiełka <224478@edu.p.lodz.pl> on 09.11.2019.
+//
+
+#include "model/Vehicle.h"
+
+Vehicle::Vehicle(const string &id, const int baseRentPrice) : id(id), baseRentPrice(baseRentPrice) {}
+
+Vehicle::~Vehicle() {}
